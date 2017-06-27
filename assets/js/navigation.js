cf97f7180205d9508be9e9ecf7df9695ea8ec626
Kevin Norris <kevin.norris.90@gmail.com>
2017-06-28 10:59:25 +1200

    Update header style

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,12 +1,1 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-console.log('navigation loaded');
-const toggleNavigation = () => {
-  let x = document.getElementById('topNav');
-  if (x.className === 'site-nav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'site-nav';
-  }
-}
 
-window.toggleNavigation = toggleNavigation;
